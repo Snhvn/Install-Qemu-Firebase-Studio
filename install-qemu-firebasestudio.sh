9f9840c5513460f85f000c2164873cca6b92b399
Snhvn <snhvnnguyen@gmail.com>
2025-04-19 19:43:19 +0700

    Update install-qemu-firebasestudio.sh

--- install-qemu-firebasestudio.sh
+++ install-qemu-firebasestudio.sh
@@ -11,6 +11,14 @@ cd Qemu
 echo -e "Vui lòng nhập iso windows/linux"
 read iso1
 wget -O win.iso $iso1
-wget -O virto.iso 
+wget -O virto.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win-0.1.271.iso
 cd
-qemu-img create -f qcow2 /home/user/Qemu/disk.qcow2 128G
+echo -e "Bạn muốn bao nhiêu disk"
+read sizedisk
+qemu-img create -f qcow2 /home/user/Qemu/disk.qcow2 $sizedisk
+clear
+echo "------------------------"
+echo "SNIPA VN"
+echo "YouTube: https://youtube.com/@snipavn205"
+echo "------------------------"
+echo "Đã Xong ✔️ giờ vào trang web Cài Qemu trên Firebase Studio - Online Notepad 'https://anotepad.com/notes/8jcm8256' có lệnh để cài win và cài mạng" 
